8cce6af2f9546684b9107963a92846fdee06c06b
Everton De Lima Cerqueira <everton.bombanet@gmail.com>
2021-09-22 15:17:31 -0300

    parte 1 fim

--- app/models/dist/negociacao.js
+++ app/models/dist/negociacao.js
@@ -2,35 +2,22 @@
 exports.__esModule = true;
 exports.Negociacao = void 0;
 var Negociacao = /** @class */ (function () {
-    function Negociacao(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    function Negociacao(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
-    Object.defineProperty(Negociacao.prototype, "data", {
-        get: function () {
-            return this._data;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Negociacao.prototype, "quantidade", {
-        get: function () {
-            return this._quantidade;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Negociacao.prototype, "valor", {
-        get: function () {
-            return this._quantidade;
-        },
-        enumerable: false,
-        configurable: true
-    });
     Object.defineProperty(Negociacao.prototype, "volume", {
         get: function () {
-            return this._quantidade * this._valor;
+            return this.quantidade * this.valor;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Negociacao.prototype, "data", {
+        get: function () {
+            var data = new Date(this._data.getTime());
+            return data;
         },
         enumerable: false,
         configurable: true
