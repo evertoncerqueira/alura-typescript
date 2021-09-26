c300c921bc9f02ede891bb9e3202c57e9879cc13
Everton De Lima Cerqueira <everton.bombanet@gmail.com>
2021-09-26 11:01:52 -0300

    parte 2 aula 2

--- app/controllers/dist/negociacao-controller.js
+++ app/controllers/dist/negociacao-controller.js
@@ -3,11 +3,13 @@ exports.__esModule = true;
 exports.NegociacaoController = void 0;
 var negociacao_js_1 = require("../models/negociacao.js");
 var negociacoes_js_1 = require("../models/negociacoes.js");
+var mensagem_view_js_1 = require("../views/mensagem-view.js");
 var negociacoes_view_js_1 = require("../views/negociacoes-view.js");
 var NegociacaoController = /** @class */ (function () {
     function NegociacaoController() {
         this.negociacoes = new negociacoes_js_1.Negociacoes();
         this.negociacoesView = new negociacoes_view_js_1.NegociacoesView('#negociacoesView');
+        this.mensagemView = new mensagem_view_js_1.MensagemView('#mensagemView');
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
@@ -17,6 +19,7 @@ var NegociacaoController = /** @class */ (function () {
         var negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
         this.limparFormulario();
     };
     NegociacaoController.prototype.criaNegociacao = function () {
